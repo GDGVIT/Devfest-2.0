3c2f3639e9476395c584b7cb05ef2e135495ed00
Vineeth S <svineth.face@gmail.com>
2017-10-01 02:26:55 +0530

    added card parallax

--- src/js/cardParallax.js
+++ src/js/cardParallax.js
@@ -3,5 +3,28 @@
  */
 
 export default  function () {
-
+    const $top=$('#devfest-logo-top');
+    $top.css('visibility','visible');
+    $('.landing-wrapper').after($top);
+    let $bottom=$('#devfest-fixed');
+    let top=$top.offset().top,bottom=$bottom.offset().top,flag=false;
+    function handleScroll(e) {
+        let scroll=e.srcElement.scrollingElement.scrollTop;
+        console.log(scroll,top,bottom);
+        if(flag){
+            if(scroll+top<bottom){
+                flag=false;
+                $bottom.css('visibility','hidden');
+                $top.css('visibility','visible');
+            }
+        }
+        else {
+            if(scroll+top>=bottom){
+                flag=true;
+                $top.css('visibility','hidden');
+                $bottom.css('visibility','visible');
+            }
+        }
+    }
+    document.onscroll=handleScroll;
 }
--- src/js/removeGlitch.js
+++ src/js/removeGlitch.js
@@ -4,7 +4,11 @@
 import $ from 'jquery';
 export default function () {
     const $rg=$('.glitch-dots');
-    $rg.eq(0).children('img').eq(0).addClass('fade-dots').siblings().remove();
-    $rg.eq(1).children('img').eq(0).addClass('fade-dots').siblings().remove();
+    $rg.eq(0).children('img').eq(0).addClass('fade-dots').css('position','fixed').siblings().remove();
+    $rg.eq(1).children('img').eq(0).addClass('fade-dots').css('position','fixed').siblings().remove();
     $rg.removeClass('glitch-dots');
+    const $lw=$('.loadWrap');
+    $lw.after($(`<a id="mouse-button" class="animated fadeInUp"><span class="mouse"><span class="mouse-scroll"></span></span></a>`));
+    $lw.remove();
+    $('.example-three').remove();
 }
